e007a74e8563e131feb93ac340253f50e6a8e5cb
Chiaki Uehira <me@uhck.in>
2019-05-19 19:36:31 +0900

    design

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
     <v-list subheader two-line class="pa-0">
-      <v-subheader>General</v-subheader>
+      <v-subheader>一覧</v-subheader>
+      <v-divider></v-divider>
       <template v-for="item in openItems">
         <div :key="item.title">
           <v-list-tile @click="">
             <v-list-tile-action>
               <v-checkbox @change="toggleStatesById(item.id)" :input-value="item.isClose"></v-checkbox>
             </v-list-tile-action>
-            <v-list-tile-content to="/1">
+            <v-list-tile-content @click="toDetail(item.id)">
               <v-list-tile-title v-html="item.title"></v-list-tile-title>
-              <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-divider></v-divider>
         </div>
       </template>
     </v-list>
-    <v-list subheader two-line>
+    <v-list　v-if="this.closeItems.length" subheader two-line>
       <v-subheader>完了</v-subheader>
+      <v-divider></v-divider>
       <template v-for="item in closeItems">
         <div :key="item.title">
           <v-list-tile @click="">
             <v-list-tile-action>
               <v-checkbox @change="toggleStatesById(item.id)" :input-value="item.isClose"></v-checkbox>
             </v-list-tile-action>
-            <v-list-tile-content to="/1">
+            <v-list-tile-content @click="toDetail(item.id)">
               <v-list-tile-title v-html="item.title"></v-list-tile-title>
-              <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
             </v-list-tile-content>
           </v-list-tile>
           <v-divider></v-divider>
@@ -43,7 +43,10 @@ export default {
   methods: {
     ...mapActions('lists', [
       'toggleStatesById'
-    ])
+    ]),
+    toDetail (id) {
+      this.$router.push('/items/' + id)
+    }
   },
   computed: {
     ...mapGetters('lists', [
