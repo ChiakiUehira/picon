5c2fd2c9807a2e06534603f6e5084f400f05dd85
Chiaki Uehira <me@uhck.in>
2019-05-19 15:01:32 +0900

    design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,16 @@
 <template>
   <v-app>
     <v-toolbar>
-    <v-toolbar-side-icon></v-toolbar-side-icon>
-    <v-toolbar-title>PICON</v-toolbar-title>
-  </v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>ToDo</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>person_add</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>add_circle</v-icon>
+      </v-btn>
+    </v-toolbar>
   <v-content>
     <v-container fluid>
       <nuxt/>
