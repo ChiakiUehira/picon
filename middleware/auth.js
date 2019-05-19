d108cf75eda31347ee34bd82675931d178fe3a3c
Chiaki Uehira <me@uhck.in>
2019-05-20 02:57:39 +0900

    design

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,9 @@ import auth from '~/plugins/auth'
 export default async function ({ store, route, redirect }) {
   return new Promise (async (resolve) => {
     const user = await auth()
-    store.dispatch('app/setUser', user)
+    await store.dispatch('app/setUser', user)
+    console.log(store.getters['app/isLogged']);
+    console.log(route.path);
     if (!store.getters['app/isLogged'] && route.path !== '/login') {
       redirect('/login')
     }
