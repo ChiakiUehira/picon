4920850f4e08b30eb84bdc0a192397a11a77e3fe
Chiaki Uehira <me@uhck.in>
2019-05-28 01:15:35 +0900

    Update

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+     ...mapGetters('app', [
+      'username',
+    ]),
+  },
+  created() {
+    this.$router.push(`/${username}`)
+  },
+}
+</script>
