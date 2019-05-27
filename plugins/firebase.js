4518ad993691d3e0c0e5ba5386c148b18ad281ec
Chiaki Uehira <me@uhck.in>
2019-05-28 01:41:36 +0900

    Update

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,9 +1,5 @@
-import Vue from 'vue'
 import firebase from 'firebase'
-import { firestorePlugin } from 'vuefire'
 import 'firebase/firestore'
-
-Vue.use(firestorePlugin)
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
